273db57580407eeae86d733fd805a64bf8fe5539
MarielaNavarro <102978496+MarielaNavarro@users.noreply.github.com>
2026-09-06 21:06:00 -0300

    Update cancion.py

--- esqueleto/src/dominio/cancion.py
+++ esqueleto/src/dominio/cancion.py
@@ -1,1 +1,8 @@
+class Cancion:
+    def __init__(self, titulo, artista, genero):
+        self.titulo = titulo
+        self.artista = artista
+        self.genero = genero
 
+    def __str__(self):
+        return f"{self.titulo} - {self.artista} ({self.genero})"
