50fc11286b3daa733a2b0b84f23a8e9449bd3d38
MarielaNavarro <102978496+MarielaNavarro@users.noreply.github.com>
2026-09-18 15:30:11 -0300

    Add files via upload

---
+++ esqueleto/src/dominio/catalogo.py
@@ -0,0 +1,10 @@
+from .cancion import Cancion
+
+
+catalogo = [
+    Cancion("Imagine", "John Lennon", "Imagine"),
+    Cancion("Billie Jean", "Michael Jackson", "Thriller"),
+    Cancion("Like a Rolling Stone", "Bob Dylan", "Highway 61 Revisited"),
+    Cancion("Bohemian Rhapsody", "Queen", "A Night at the Opera"),
+    Cancion("Hotel California", "Eagles", "Hotel California"),
+]
